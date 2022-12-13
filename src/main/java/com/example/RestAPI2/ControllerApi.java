5eeaa54510881598441d81ccc84043dc298d39b9
coepavi <swamiaws@gmail.com>
2022-12-12 21:58:42 -0800

    GET & POST Springboot API

---
+++ src/main/java/com/example/RestAPI2/ControllerApi.java
@@ -0,0 +1,24 @@
+package com.example.RestAPI2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class ControllerApi {
+
+	@GetMapping("/getdata")
+	public String getdata() {
+		System.out.println("Get API triggered");
+		return ("Get Api sent data back");
+	}
+	
+	@PostMapping("/postdata")
+	public String postdata(@RequestBody EntityPerson entityPerson) {
+		System.out.println("Post API triggered" + entityPerson.getFullname());
+		return ("POST Api sent data back");
+	}
+
+}
---
+++ src/main/java/com/example/RestAPI2/EntityPerson.java
@@ -0,0 +1,29 @@
+package com.example.RestAPI2;
+
+public class EntityPerson {
+
+	private int id;
+	private String fullname;
+	private String state;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	
+}
